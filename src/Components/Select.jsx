c1551ae2c966ba7e05860f10765e0bfd885c054e
vinoth <vinoth@gmail.com>
2024-02-03 20:00:10 +0530

    solved css problem

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -4,7 +4,8 @@ const Select = () => {
   return (
     <div className="">
       <select
-      style={{  backgroundColor:" #282828",color:"#ffffff"
+      style={{  backgroundColor:" #282828",color:"#ffffff",  border: "3px solid #ff4233"
+
 }}
         className="col-12  col-md-6 col-lg-4 form-control form width"
         required
